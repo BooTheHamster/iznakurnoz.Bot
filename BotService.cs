43158a7cd00cab04535d822984e096e30cdb8bf3
Oleg Sevostyanov <oleg.sev@gmail.com>
2020-02-07 10:46:00 +0400

    Добавлена заготовка для бота

--- BotService.cs
+++ BotService.cs
@@ -4,6 +4,8 @@ using System.Threading.Tasks;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
+using Telegram.Bot;
+using Telegram.Bot.Args;
 
 namespace Iznakurnoz.Bot
 {
@@ -11,6 +13,7 @@ namespace Iznakurnoz.Bot
     {
         private readonly ILogger _logger;
         private readonly IOptions<BotConfig> _config;
+        private static TelegramBotClient _client;
 
         public BotService(
             ILogger<BotService> logger,
@@ -23,17 +26,30 @@ namespace Iznakurnoz.Bot
         public Task StartAsync(CancellationToken cancellationToken)
         {
             _logger.LogInformation("Starting bot.");
+
+            _client = new TelegramBotClient(_config.Value.AuthToken);
+            _client.OnMessage += BotOnMessageReceived;
+            _client.OnMessageEdited += BotOnMessageReceived;
+            _client.StartReceiving();
+
             return Task.CompletedTask;
+        }
+
+        private void BotOnMessageReceived(object sender, MessageEventArgs e)
+        {
+
         }
 
         public Task StopAsync(CancellationToken cancellationToken)
         {
             _logger.LogInformation("Stopping bot.");
+            _client.StopReceiving();
             return Task.CompletedTask;
         }
 
         public void Dispose()
         {
+            _client = null;
             _logger.LogInformation("Disposing ...");
         }
     }
