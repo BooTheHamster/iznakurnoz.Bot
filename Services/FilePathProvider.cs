fa7a6bd2d8fc134649372268d749d786fd51accc
Oleg Sevostyanov <oleg.sev@gmail.com>
2020-04-26 08:57:16 +0400

    Использование настроек из /etc в режиме демона

--- Services/FilePathProvider.cs
+++ Services/FilePathProvider.cs
@@ -1,5 +1,6 @@
 using System;
 using System.IO;
+using System.Runtime.InteropServices;
 
 namespace iznakurnoz.Bot.Services
 {
@@ -8,6 +9,7 @@ namespace iznakurnoz.Bot.Services
     /// </summary>
     public class FilePathProvider
     {
+        private const string DaemonCommandLineFlag = "--daemon";
         private const string BotFolderName = "iznakurnozbot";
 
         /// <summary>
@@ -26,7 +28,25 @@ namespace iznakurnoz.Bot.Services
         /// <returns>Полный путь к каталогу с файлами.</returns>
         public static string GetDirectoryPath()
         {
-            var folder = Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData);
+            var asDaemon = Environment.CommandLine.Contains(DaemonCommandLineFlag);
+            var folder = string.Empty;
+
+            if (RuntimeInformation.IsOSPlatform(OSPlatform.Linux))
+            {
+                if (asDaemon)
+                {
+                    folder = "/etc";
+                }
+                else
+                {
+                    // Для отладки не в режиме демона будет использоваться конфиг из локального каталога пользователя.
+                    folder = Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData);
+                }
+            } else if (RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
+            {
+                folder = Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData);
+            }
+
             folder = Path.Combine(folder, BotFolderName);
             return folder;
         }
